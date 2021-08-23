7a8ebdb0fe98ae78ae1af95506c0d52eaa383ceb
sikegotcha <yamakasy1337@gmail.com>
2021-08-24 03:48:58 +0500

    task 1.6: unicafe step1 is completed

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+
+const Button = ({onClick, text}) => {
+ 
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({good, bad, neutral}) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodFeedback = () => setGood(good + 1)
+  const neutralFeedBack = () => setNeutral(neutral + 1)
+  const badFeedBack = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <Button onClick={goodFeedback} text='good'/>
+      <Button onClick={neutralFeedBack} text='neutral'/>
+      <Button onClick={badFeedBack} text='bad'/>
+
+      <h1>statistics</h1>
+
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
