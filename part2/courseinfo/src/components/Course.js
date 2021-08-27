fd7e385d39e7425990c5dbf3da8394a7ac3678c6
sikegotcha <yamakasy1337@gmail.com>
2021-08-27 05:13:54 +0500

    exercise 2.4 is completed

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,14 +2,12 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-        </div>
-    )
-}
+const Course = ({ course }) => (
+    <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+    </div>
+)
 
 export default Course
