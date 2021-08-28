56bbc5eefd41e9a49ca7eb0b86ac2da6cd72ea42
sikegotcha <yamakasy1337@gmail.com>
2021-08-28 23:12:14 +0500

    exercises 2.9 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ findPerson, setFindPeson ] = useState('')
 
 
   const addName = (event) => {
@@ -15,7 +18,7 @@ const App = () => {
     const sameNumber = persons.some(person => person.number === newNumber)
 
     if(samePerson || sameNumber) {
-      alert(newName + " is already in a phonebook, or " + newNumber + " is already used")
+      alert("that name or the phone is already in a phonebook")
       setNewName('');
       setNewNumber('');
     } else {
@@ -35,19 +38,38 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFindPerson = (event) => {
+    console.log(event.target.value)
+    setFindPeson(event.target.value)  
+  }
+
+
 
   
   const DisplayPersons = ({persons}) => {
-    return (
-      <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-      </div>
-    )
+    if(findPerson === '') {
+      return (
+        <div>
+          {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        </div>
+      )
+    } else {
+      const foundPeople = persons.filter(person => person.name.toLowerCase().includes(findPerson.toLowerCase()))
+      return (
+        <div>
+         {foundPeople.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        </div>
+      )
+    }
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <div>
+        <h2>Phonebook</h2>
+        filter shown with<input value={findPerson} onChange={handleFindPerson}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleAddName}/>
@@ -57,7 +79,7 @@ const App = () => {
         </div>
         <div>
           <button type="submit">add</button>
-        </div>
+        </div>  
       </form>
       <h2>Numbers</h2>
       <DisplayPersons persons={persons}/>
